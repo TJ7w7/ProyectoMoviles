b27af8afe8dbedc6845df924456dbebbb4f1c809
JuniorTomas <juniortomasarenas24@gmail.com>
2025-11-13 13:16:55 -0500

    arreglo de apartade de mis asignaciones

--- app/src/main/java/com/tj/proyecto/Recolector/MisAsignaciones.kt
+++ app/src/main/java/com/tj/proyecto/Recolector/MisAsignaciones.kt
@@ -1,6 +1,7 @@
 package com.tj.proyecto.Recolector
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -26,7 +27,6 @@ class MisAsignaciones : Fragment() {
 
     private val asignacionesList = mutableListOf<entAsignacionRuta>()
     private lateinit var adapter: MisAsignacionesAdapter
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
